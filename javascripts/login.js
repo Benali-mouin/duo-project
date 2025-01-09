3984bb6c771d34274e834c429d12170efce7f08a
Benali-mouin <benalimouin3@gmail.com>
2025-01-09 22:00:34 +0100

    last version

---
+++ javascripts/login.js
@@ -0,0 +1,36 @@
+$('document').ready(function(){
+
+    var users = JSON.parse(localStorage.getItem('users')) || [];
+    console.log(users);
+
+    $('#login').on("click", function() {
+        
+        var username = $('.username').val();
+        var password = $('.password').val();
+        console.log({username, password});
+
+        // Function to check if the user exists in localStorage
+        function checkUserExists(username, password) {
+            for (var i = 0; i < users.length; i++) {
+                if (users[i].username === username && users[i].password === password) {
+                    return true; // User exists
+                }
+            }
+            return false; // User does not exist
+        }
+
+        if (checkUserExists===true) {
+            alert('Login successful! Redirecting to the next page...');
+            window.location.href = '/htmls/index3.html';
+        } else {
+            alert('Username or password does not exist. Please try again.');
+            // Stay on the current page
+        }
+    })
+
+    $('.sign up').click(function (e) {
+        e.preventDefault();
+        alert('Redirecting to the sign-up page...');
+        window.location.href = '/htmls/index2.html';
+    });
+});
--- javascripts/mouin.js
+++ javascripts/mouin.js
@@ -1,50 +1,32 @@
 
 $('document').ready(function(){
 
-
     var users = JSON.parse(localStorage.getItem('users'))||[]
-console.log(users);
+console.log(users)
 
-    
-    
-    
-    $('#login').on("click",function() {// click login button
+     $('#login').on("click",function() {
         
         var username = $('.username').val()
         var password = $('.password').val()
-    console.log({username,password});
+    console.log({username,password})
     
-        
         var user = {
             username,password
         }
         users.push(user)
         localStorage.setItem("users",JSON.stringify(users))
         alert('Login successful! Redirecting to the next page...')
-
-        if((username!==username)&&(password!==password)) {
-            alert('please enter your correct infos')
-        }
     
         window.location.href = '/htmls/index3.html'
     })
-    
-    
-    
-    
+
     $('.sign up').click(function (e) {
         e.preventDefault()
         alert('Redirecting to the sign-up page...')
         window.location.href = '/htmls/index2.html'
     })
-    
-    
-    
-    
-    
-    
-    
-    
-
 })
+
+
+
 
