5d2cfc20ffbe2bdce6f3eb45d7444006c3ecf1a1
@22Nour-Ai <issaouinour853@gmail.com>
2025-01-09 14:31:29 +0100

    pair

---
+++ javascripts/nour.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+    $("#categories-btn").click(function () {
+        $("#categories-dropdown").toggle();
+    });
+
+    $("#my-list").click(function () {
+        alert("favorite movies selected!");
+    });
+});
+
+
