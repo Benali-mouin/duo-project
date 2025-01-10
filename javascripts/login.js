d0cd7033dcf6f14b4268437f7989faf23ac22100
Benali-mouin <benalimouin3@gmail.com>
2025-01-10 13:09:53 +0100

    tes2

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,36 +1,38 @@
 $('document').ready(function(){
 
-    var users = JSON.parse(localStorage.getItem('users')) || [];
-    console.log(users);
+    var users = JSON.parse(localStorage.getItem('users')) || []
+    console.log(users)
 
     $('#login').on("click", function() {
         
-        var username = $('.username').val();
-        var password = $('.password').val();
-        console.log({username, password});
+        var username = $('.username').val()
+        var password = $('.password').val()
+        console.log({username, password})
 
         // Function to check if the user exists in localStorage
         function checkUserExists(username, password) {
-            for (var i = 0; i < users.length; i++) {
+            for (var i=0;i<users.length;i++) {
+                console.log(users[i]);
+                
                 if (users[i].username === username && users[i].password === password) {
-                    return true; // User exists
+                    return true // User exists
                 }
             }
-            return false; // User does not exist
+            return false // User does not exist
         }
 
-        if (checkUserExists===true) {
-            alert('Login successful! Redirecting to the next page...');
-            window.location.href = '/htmls/index3.html';
+        if (checkUserExists(username,password)===true) {
+            alert('Login successful! Redirecting to the next page...')
+            window.location.href ='/htmls/home.html'
         } else {
-            alert('Username or password does not exist. Please try again.');
+            alert('Username or password does not exist. Please try again.')
             // Stay on the current page
         }
     })
 
     $('.sign up').click(function (e) {
-        e.preventDefault();
-        alert('Redirecting to the sign-up page...');
-        window.location.href = '/htmls/index2.html';
-    });
-});
+        e.preventDefault()
+        alert('Redirecting to the sign-up page...')
+        window.location.href ='/htmls/index2.html'
+    })
+})
